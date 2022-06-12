3ef9adbf851d9d06569d3ea81f171678bccc804c
FAFCHAMPS Bastien <b.fafchamps@student.helmo.be>
2022-06-12 14:01:43 +0200

    Added syntax highlighter rules

--- src/js/codeParser.js
+++ src/js/codeParser.js
@@ -4,7 +4,7 @@ export class CodeParser {
             { regex: /"[^"\\]*"/, type: 'string_0', tags: ['string'] },
             { regex: /'[^'\\]*'/, type: 'string_1', tags: ['string'] },
             { regex: /\s+/, type: 'whitespace', tags: ['whitespace'] },
-            { regex: /(false|true)/, type: 'boolean', tags: ['boolean']},
+            { regex: /(false|true)/, type: 'boolean', tags: ['boolean'] },
             { regex: /[0-9]+(.[0-9]+)?/, type: 'number', tags: ['number'] },
             { regex: /\/\/[^\n\r]+/, type: 'comment_0', tags: ['comment'] },
             { regex: /\/\*(\*\/)*\*\//, type: 'comment_1', tags: ['comment'] },
@@ -32,7 +32,7 @@ export class CodeParser {
             { regex: /class /, type: 'keyword_class', tags: ['keyword'] },
             { regex: /null/, type: 'keyword_null', tags: ['keyword'] },
             { regex: /\>\=/, type: 'comparator_greaterequals', tags: ['comparator'] },
-            { regex: /\<\=/, type: 'comparator_smallerequals', tags: ['comparator']},
+            { regex: /\<\=/, type: 'comparator_smallerequals', tags: ['comparator'] },
             { regex: /\!=/, type: 'comparator_notequals', tags: ['comparator'] },
             { regex: /\&\&/, type: 'comparator_and', tags: ['comparator'] },
             { regex: /\|\|/, type: 'comparator_or', tags: ['comparator'] },
@@ -47,29 +47,80 @@ export class CodeParser {
             { regex: /[a-zA-Z_][a-zA-Z_0-9]*/, type: 'identifier', tags: ['identifier'] },
         ];
 
+        this.RULES = [
+            {
+                'type': 'identifier',
+                'previous': [],
+                'next': ['separator_parentheseopen'],
+                'result': 'function_call'
+            },
+            {
+                'type': 'identifier',
+                'previous': ['keyword_function'],
+                'next': ['separator_parentheseopen'],
+                'result': 'function',
+                remove: ['function_call']
+            },
+            {
+                'type': 'identifier',
+                'previous': [],
+                'next': ['punctuation_doubledot'],
+                'result': 'object'
+            }
+        ]
+
         let regex = this.__getFullRegex();
         this.matcher = new RegExp(regex, 'g');
     }
 
     parseTokens(code) {
         let matches = code.matchAll(this.matcher);
-        let results = [];
+        let tokens_all = [];
+        let tokens = [];
 
         for (const match of matches) {
             let groups = match.groups
             for (const key in groups) {
-                if (groups[key] != null)
-                {
-                    results.push({
-                        'value': groups[key],
-                        'identifier': key,
-                        'tags': this.TOKENS_MAP[key].tags,
-                    });
+                if (groups[key] != null) {
+                    let token ={
+                        value: groups[key],
+                        type: key,
+                        tags: [...this.TOKENS_MAP[key].tags],
+                    };
+
+                    tokens_all.push(token);
+                    if (key != 'whitespace')
+                        tokens.push(token);
+                    
                     break;
                 }
             }
         }
-        return results;
+
+        for (let i = 0; i < tokens.length; i++) {
+            let token = tokens[i];
+
+            this.RULES.forEach(rule => {
+                if (token.type != rule.type)
+                    return;
+
+                for (let j = 0; j < rule.next.length && i + j + 1 < tokens.length; j++) {
+                    if (tokens[i + j + 1].type != rule.next[j])
+                        return;
+                }
+
+                for (let j = 0; j < rule.previous.length && i - j - 1 >= 0; j++) {
+                    if (tokens[i - j - 1].type != rule.previous[j])
+                        return;
+                }
+                token.tags.push(rule.result);
+
+                if (rule.remove != null)
+                    token.tags = token.tags.filter(tag => !rule.remove.includes(tag));
+            });
+        }
+
+        return tokens_all;
     }
 
     translate(tokens) {
